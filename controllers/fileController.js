674c9b913bbf5d9c8961b2dcafcd0ac93e6d537b
Rahman Younus <rahman_95@live.co.uk>
2017-04-15 17:30:53 +0100

    Got variables passing from controller to view working

--- controllers/fileController.js
+++ controllers/fileController.js
@@ -1,7 +1,18 @@
 module.exports.controller = function(app) {
 
   app.get('/', function (req, res) {
-    res.render('files');
+    var folders = getFolders();
+
+    data = {folder: folders};
+    res.render('files/index', data);
   })
+
+  function getFolders(){
+    return '12345';
+  }
+
+  function getFiles(folder){
+    return 'files';
+  }
 
 }
